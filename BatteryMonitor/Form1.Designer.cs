5b527f9a49cd737d60ca5009b2381724d84a6674
user1 <user1@450G46CD7180MNQ>
2017-10-15 07:12:16 +0400

    Added header text

--- BatteryMonitor/Form1.Designer.cs
+++ BatteryMonitor/Form1.Designer.cs
@@ -104,7 +104,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Battery Monitor";
             this.ResumeLayout(false);
             this.PerformLayout();
 
